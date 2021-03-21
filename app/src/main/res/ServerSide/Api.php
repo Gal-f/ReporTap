a89c9dfb736df4420c904b52bb35bcfae70e486f
Gal-f <galfroind@gmail.com>
2021-03-21 15:08:09 +0200

    PHP files: Changed Api.php & DbConnect.php to a class form, in order to access methods from different PHP files. Added NewMessage.java initial logic for new message form (without sending yet).

--- app/src/main/res/ServerSide/Api.php
+++ app/src/main/res/ServerSide/Api.php
@@ -1,128 +1,143 @@
 <?php 
 
-	require_once 'DbConnect.php';
+    class Api{
 	
-	$response = array();
+        private $conn;
+        private $response;
+        
+        function __construct(){
+            require_once 'DbConnect.php';
+            //require_once dirname(__FILE__).'/DbConnect.php';
+            
+            $db = new DbConnect();
+            $this->conn = $db->connect();
+            $this->response = array();
+        }
 	
-	if(isset($_GET['apicall'])){
+	
+        if(isset($_GET['apicall'])){
 		
-		switch($_GET['apicall']){
+            switch($_GET['apicall']){
 			
-			case 'signup':
-				if(isTheseParametersAvailable(array('username','password','employee_ID','full_name','role','phone_number','works_in_dept'))){
-					//getting the values 
-					$username = $_POST['username']; 
-					$password = md5($_POST['password']);
-					$employeeNumber = $_POST['employee_ID']; 
-					$fullName = $_POST['full_name']; 
-					$jobTitle = $_POST['role']; 
-					$phoneNumber = $_POST['phone_number']; 
-					$deptID = $_POST['works_in_dept']; 
-					
-					//checking if the user is already exist with this username
-					$stmt = $conn->prepare("SELECT id FROM users WHERE username = ?");
-					$stmt->bind_param("s", $username);
-					$stmt->execute();
-					$stmt->store_result();
-					
-					//if the user already exist in the database 
-					if($stmt->num_rows > 0){
-						$response['error'] = true;
-						$response['message'] = 'User already registered';
-						$stmt->close();
-					}else{
-						$stmt = $conn->prepare("INSERT INTO users (username, password, employee_ID, full_name, role, phone_number, works_in_dept) VALUES (?, ?, ?, ?, ?, ?, ?)");
-						$stmt->bind_param("ssssssi", $username, $password, $employeeNumber, $fullName, $jobTitle, $phoneNumber, $deptID);
+                case 'signup':
+                    if(isTheseParametersAvailable(array('password','employee_ID','full_name','role','phone_number','works_in_dept'))){
+                        //getting the values 
+                        $password = md5($_POST['password']);
+                        $employeeNumber = $_POST['employee_ID']; 
+                        $fullName = $_POST['full_name']; 
+                        $jobTitle = $_POST['role']; 
+                        $phoneNumber = $_POST['phone_number']; 
+                        $deptID = $_POST['works_in_dept']; 
 
-						if($stmt->execute()){
-							$stmt = $conn->prepare("SELECT id, username, employee_ID, full_name, role, phone_number, works_in_dept  FROM users WHERE username = ?"); 
-							$stmt->bind_param("s",$username);
-							$stmt->execute();
-							$stmt->bind_result($id, $username, $employeeNumber, $fullName, $jobTitle, $phoneNumber, $deptID);
-							$stmt->fetch();
+                        //checking if the user is already exist with this employee number (must be unique) 
+                        $stmt = $this->conn->prepare("SELECT id FROM users WHERE employee_ID = ? OR phone_number = ?");
+                        $stmt->bind_param("ss", $employeeNumber, $phoneNumber);
+                        $stmt->execute();
+                        $stmt->store_result();
+
+                        //if the user already exist in the database 
+                        if($stmt->num_rows > 0){
+                            $this->response['error'] = true;
+                            $this->response['message'] = 'משתמש קיים במערכת, יש לבדוק את מספר העובד ואת מספר הטלפון שהוזנו';
+                            $stmt->close();
+                        }else{
+                            $stmt = $this->conn->prepare("INSERT INTO users (password, employee_ID, full_name, role, phone_number, works_in_dept) VALUES (?, ?, ?, ?, ?, ?)");
+                            $stmt->bind_param("sssssi", $password, $employeeNumber, $fullName, $jobTitle, $phoneNumber, $deptID);
+
+                            if($stmt->execute()){
+                                $stmt = $this->conn->prepare("SELECT id, employee_ID, full_name, role, phone_number, works_in_dept  FROM users WHERE employee_ID = ?"); 
+                                $stmt->bind_param("s",$employeeNumber);
+                                $stmt->execute();
+                                $stmt->bind_result($id, $employeeNumber, $fullName, $jobTitle, $phoneNumber, $deptID);
+                                $stmt->fetch();
+                                
+                                $user = array(
+                                    'id'=>$id, 
+                                    'employee_ID' => $employeeNumber,
+                                    'full_name' => $fullName,
+                                    'role' => $jobTitle,
+                                    'phone_number' => $phoneNumber,
+                                    'works_in_dept' => $deptID
+                                );
 							
-							$user = array(
-								'id'=>$id, 
-								'username'=>$username, 
-								'employee_ID' => $employeeNumber,
-								'full_name' => $fullName,
-								'role' => $jobTitle,
-								'phone_number' => $phoneNumber,
-								'works_in_dept' => $deptID
-							);
-							
-							$stmt->close();
-							//adding the user data in response 
-							$response['error'] = false; 
-							$response['message'] = 'User registered successfully'; 
-							$response['user'] = $user; 
-						}
-					}
-					
-				}else{
-					$response['error'] = true; 
-					$response['message'] = 'required parameters are not available'; 
-				}
+                                $stmt->close();
+                                //adding the user data in response 
+                                $this->response['error'] = false; 
+                                $this->response['message'] = 'User registered successfully'; 
+                                $this->response['user'] = $user; 
+                            }
+                        }
+                    }
+                    break; 
+                
+                case 'login':
 				
-			break; 
+                    if(isTheseParametersAvailable(array('employee_ID', 'password'))){
+					
+                        $employeeNumber = $_POST['employee_ID'];
+                        $password = md5($_POST['password']); 
+					
+                        $stmt = $this->conn->prepare("SELECT id, employee_ID, full_name, role, phone_number, works_in_dept FROM users WHERE employee_ID = ? AND password = ?");
+                        $stmt->bind_param("ss",$employeeNumber, $password);
+					
+                        $stmt->execute();
+					
+                        $stmt->store_result();
+					
+                        if($stmt->num_rows > 0){
+						
+                            $stmt->bind_result($id, $employeeNumber, $fullName, $jobTitle, $phoneNumber, $deptID);
+                            $stmt->fetch();
+						
+                            $user = array(
+                                'id'=>$id, 
+                                'employee_ID'=>$employeeNumber,
+                                'full_name' => $fullName,
+                                'role' => $jobTitle,
+                                'phone_number' => $phoneNumber,
+                                'works_in_dept' => $deptID
+                            );
+						
+                            $this->response['error'] = false; 
+                            $this->response['message'] = 'Login successfull'; 
+                            $this->response['user'] = $user; 
+                        }else{
+                            $this->response['error'] = false; 
+                            $this->response['message'] = 'שגיאה בפרטי ההזדהות';
+                        }
+                    }
+                    break; 
+                
+                case 'newmessage':
+                    if (isTheseParametersAvailable(array('','',''))){
+                        //Call external function from DbOperations.php
+                    }
+                    break;
 			
-			case 'login':
-				
-				if(isTheseParametersAvailable(array('username', 'password'))){
-					
-					$username = $_POST['username'];
-					$password = md5($_POST['password']); 
-					
-					$stmt = $conn->prepare("SELECT id, username, employee_ID, full_name, role, phone_number, works_in_dept FROM users WHERE username = ? AND password = ?");
-					$stmt->bind_param("ss",$username, $password);
-					
-					$stmt->execute();
-					
-					$stmt->store_result();
-					
-					if($stmt->num_rows > 0){
-						
-						$stmt->bind_result($id, $username, $employeeNumber, $fullName, $jobTitle, $phoneNumber, $deptID);
-						$stmt->fetch();
-						
-						$user = array(
-							'id'=>$id, 
-							'username'=>$username, 
-							'employee_ID'=>$employeeNumber,
-							'full_name' => $fullName,
-							'role' => $jobTitle,
-							'phone_number' => $phoneNumber,
-							'works_in_dept' => $deptID
-						);
-						
-						$response['error'] = false; 
-						$response['message'] = 'Login successfull'; 
-						$response['user'] = $user; 
-					}else{
-						$response['error'] = false; 
-						$response['message'] = 'Invalid username or password';
-					}
-				}
-			break; 
-			
-			default: 
-				$response['error'] = true; 
-				$response['message'] = 'Invalid Operation Called';
-		}
-		
-	}else{
-		$response['error'] = true; 
-		$response['message'] = 'Invalid API Call';
-	}
+                default: 
+                    $this->response['error'] = true; 
+                    $this->response['message'] = 'Invalid Operation Called';
+            }
+        }else{
+            $this->response['error'] = true; 
+            $this->response['message'] = 'Invalid API Call';
+        }
+        
+        echo json_encode($this->response);
 	
-	echo json_encode($response);
-	
-	function isTheseParametersAvailable($params){
+        
+        function isTheseParametersAvailable($params){
 		
-		foreach($params as $param){
-			if(!isset($_POST[$param])){
-				return false; 
-			}
-		}
-		return true; 
-	}
+            $error = false;
+            foreach($params as $param){
+                if(!isset($_POST[$param])){ // Whether a parameter is missing
+                    $error = true; 
+                }
+            }
+            if ($error){    // If there's an error, set the response to 'missing parameters' error message
+                $this->response['error'] = true; 
+                $this->response['message'] = 'required parameters are not available'; 
+            }
+            return !($error); // Return true if all parameters are in order
+        }
+    }
