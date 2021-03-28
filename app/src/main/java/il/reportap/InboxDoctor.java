6ff3bacbc160862ee7967b1f62f6e5f5f573fba9
DevByYael <79448347+DevByYael@users.noreply.github.com>
2021-03-28 20:31:10 +0300

    Inbox Doctor Dubug

--- app/src/main/java/il/reportap/InboxDoctor.java
+++ app/src/main/java/il/reportap/InboxDoctor.java
@@ -1,15 +1,16 @@
 package il.reportap;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -22,12 +23,16 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.time.Clock;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class InboxDoctor extends AppCompatActivity {
 
-        private static final String URL = "http://androidcodefinder.com/RecyclerViewJson.json";
+       // private static final String URL = "http://androidcodefinder.com/RecyclerViewJson.json";
+    private URLs url;
         private RecyclerView recyclerView;
         private RecyclerView.Adapter adapter;
         private List<ModelActivity> modelActivityList;
@@ -36,7 +41,7 @@ public class InboxDoctor extends AppCompatActivity {
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main);
+            setContentView(R.layout.inbox_doctor);
 
             relativeLayout = (RelativeLayout) findViewById(R.id.relativeLayout);
             recyclerView = (RecyclerView)findViewById(R.id.recyclerView);
@@ -51,7 +56,7 @@ public class InboxDoctor extends AppCompatActivity {
         private void loadData() {
 
             StringRequest stringRequest = new StringRequest(Request.Method.GET,
-                    URL,
+                    url.URL_INBOXDR,
                     new Response.Listener<String>() {
                         @Override
                         public void onResponse(String response) {
@@ -87,12 +92,21 @@ public class InboxDoctor extends AppCompatActivity {
                     new Response.ErrorListener() {
                         @Override
                         public void onErrorResponse(VolleyError error) {
-
+                            Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_LONG).show();
                         }
-                    });
+                    })
+            {
+                @Nullable
+                @Override
+                protected Map<String,String> getParams() throws AuthFailureError {
+                    Map<String,String> params = new HashMap<>();
+                    params.put("department", String.valueOf(SharedPrefManager.getInstance(getApplicationContext()).getUser().getDepartment()));
 
+                    return params;
+                }
+            };
             RequestQueue requestQueue = Volley.newRequestQueue(this);
             requestQueue.add(stringRequest);
-        }
 
+}
 }
