90c39b00104c27afb279d05a1f6fd38428350da9
DevByYael <79448347+DevByYael@users.noreply.github.com>
2021-04-16 10:27:07 +0300

    linked inboxDr to sentDr

---
+++ app/src/main/java/il/reportap/AdapterActivitySentDr.java
@@ -0,0 +1,72 @@
+package il.reportap;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.graphics.PorterDuff;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.loginregister.R;
+
+import java.util.List;
+
+public class AdapterActivitySentDr extends RecyclerView.Adapter<AdapterActivitySentDr.ViewHolder>{
+    private List<ModelActivitySentDr> modelActivitySentDrList;
+    private Context context;
+
+    public AdapterActivitySentDr(List<ModelActivitySentDr> modelActivitySentDrList, Context context) {
+        this.modelActivitySentDrList = modelActivitySentDrList;
+        this.context = context;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.row_layout_sent,parent,false);
+        return new ViewHolder(v);
+    }
+
+    @SuppressLint("ResourceAsColor")
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+            ModelActivitySentDr modelActivitySentDr = modelActivitySentDrList.get(position);
+
+            holder.sentTime.setText(modelActivitySentDr.getSent_time());
+            holder.patientId.setText(modelActivitySentDr.getPatient_id());
+            holder.testName.setText(modelActivitySentDr.getTest_name());
+            holder.text.setText(modelActivitySentDr.getText());
+            holder.senderName.setText(modelActivitySentDr.getSender_name());
+            if (!modelActivitySentDr.getConfirm_time().equals("0"))
+            {
+                holder.isChecked.setColorFilter(ContextCompat.getColor(context, R.color.lightgreen),
+                        PorterDuff.Mode.MULTIPLY);
+            }
+    }
+
+    @Override
+    public int getItemCount() {
+        return modelActivitySentDrList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        public TextView patientId, text, sentTime,senderName,testName;
+        public ImageView isChecked;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+
+            sentTime = (TextView)itemView.findViewById(R.id.sentTimeS);
+            patientId = (TextView)itemView.findViewById(R.id.patientIdS);
+            testName = (TextView)itemView.findViewById(R.id.testNameS);
+            text = (TextView)itemView.findViewById(R.id.RepText);
+            isChecked=(ImageView)itemView.findViewById(R.id.isChecked);
+            senderName=(TextView)itemView.findViewById(R.id.senderName);
+        }
+    }
+}
---
+++ app/src/main/java/il/reportap/SentDoctor.java
@@ -0,0 +1,113 @@
+package il.reportap;
+
+import android.annotation.SuppressLint;
+import android.graphics.PorterDuff;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.loginregister.R;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class SentDoctor extends OptionsMenu {
+
+    private RecyclerView recyclerView;
+    private AdapterActivitySentDr adapter;
+    private List<ModelActivitySentDr> modelActivitySentDrList;
+
+    @SuppressLint("ResourceAsColor")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sent_doctor);
+        Button btn = (Button)findViewById(R.id.sentB);
+        btn.setBackgroundColor(ContextCompat.getColor(getApplicationContext(), R.color.stroke));
+        btn.setTextColor(ContextCompat.getColor(getApplicationContext(), R.color.white));
+
+        recyclerView = (RecyclerView)findViewById(R.id.recyclerViewSentDr);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+       // DividerItemDecoration divider = new DividerItemDecoration(getApplicationContext(), DividerItemDecoration.VERTICAL);
+        recyclerView.addItemDecoration(new DividerItemDecoration(getApplicationContext(), DividerItemDecoration.VERTICAL));
+        modelActivitySentDrList = new ArrayList<>();
+        StringRequest stringRequest = new StringRequest(Request.Method.POST,
+                URLs.URL_SENTDR,
+                //lambda expression
+                response -> {
+
+
+                    try {
+                        JSONObject repObj = new JSONObject(response);
+                        JSONArray repArray = repObj.getJSONArray("report");
+
+                        for(int i=0; i<repArray.length(); i++){
+
+                            JSONObject jObg = new JSONObject();
+                            jObg= repArray.getJSONObject(i);
+                            ModelActivitySentDr modelActivitySentDr = new ModelActivitySentDr(jObg.getInt("id"),
+                                    jObg.getString("sent_time"),
+                                    jObg.getString("text"),
+                                    jObg.getString("sender_name"),
+                                    jObg.getString("patient_id"),
+                                    jObg.getString("name"),
+                                    jObg.getString("confirm_time"));
+                            modelActivitySentDrList.add(modelActivitySentDr);
+                            System.out.println(modelActivitySentDrList.get(i).getId());
+                        }
+
+
+
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+                    adapter = new AdapterActivitySentDr(modelActivitySentDrList,getApplicationContext());
+                    recyclerView.setAdapter(adapter);
+
+                },
+                //lambda expression
+                error -> Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_LONG).show())
+        {
+            @Nullable
+            @Override
+            protected Map<String,String> getParams() throws AuthFailureError {
+                Map<String,String> params = new HashMap<>();
+                params.put("works_in_dept", String.valueOf(SharedPrefManager.getInstance(getApplicationContext()).getUser().getDepartment()));
+                System.out.println(params.get("works_in_dept"));
+                return params;
+            }
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+
+
+    }
+
+
+
+
+}
