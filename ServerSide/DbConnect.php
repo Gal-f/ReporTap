197e08aa4d5999295b970de69abd7c7c18e70470
edenperetz <79195496+edenperetz@users.noreply.github.com>
2021-06-12 23:24:40 +0300

    updated server side files

--- ServerSide/DbConnect.php
+++ ServerSide/DbConnect.php
@@ -11,25 +11,16 @@ class DbConnect
     function connect()
     {
         $servername = "localhost";
-       // $username = "edenpe_eden";              //old server
         $username = "edenpeis_eden";          //new server
         $password = "edengalyael";
-      //  $database = "edenpe_ReporTapDB";        //old server
         $database = "edenpeis_ReporTapDB";    //new server
 
-        /*
-        $servername = "localhost";
-        $username = "root";
-        $password = "";
-        $database = "android";
-        */
-
-        //creating a new connection object using mysqli 
+        //creating a new connection object using mysqli
         mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT); //This configures PHP and MySQLi to send full MySQL errors
         $this->conn = new mysqli($servername, $username, $password, $database);
 
         //if there is some error connecting to the database
-        //with 'die' we will stop the further execution by displaying a message about the error 
+        //with 'die' we will stop the further execution by displaying a message about the error
         if ($this->conn->connect_error) {
             die("Connection failed: " . $this->conn->connect_error);
         }
